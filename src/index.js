5f758d2b9efcc6c240dd8700ba66fe92ceeb573e
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2019-10-23 18:28:04 -0500

    updated small business app

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDV2dQNCMzPRRHQ0lx6EUT1TJe2-77y_o4
+&callback=initMap"
+type="text/javascript">
+</script>
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
